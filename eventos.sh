ee3657597f8ed8078891f8aadfc19e06d76dc4c3
FJSFerreira <fjsferreira@gmail.com>
2021-08-13 20:06:26 +0100

    Update eventos.sh

--- eventos.sh
+++ eventos.sh
@@ -5,13 +5,10 @@ ICS_URL="https://calendar.google.com/calendar/ical/calendario.eventos.pt%40gmail
 LIST_URL="https://project-gc.com/Tools/ComingEvents?country=Portugal&submit=Filter"
 
 cd $WORKING_DIR
-curl -s $ICS_URL | grep -o 'GC.....' > calendario.txt
-curl -s $LIST_URL | xmllint --html --xpath "//tbody//a/text()" - 2> /dev/null | awk '{while(length($0)>=7){print substr($0,1,7);$0=substr($0,8)}}' > eventos.txt
+curl -s $ICS_URL | grep -o 'GC.....' | sort > calendario.txt
+curl -s $LIST_URL | xmllint --html --xpath "//tbody//a/text()" - 2> /dev/null | awk '{while(length($0)>=7){print substr($0,1,7);$0=substr($0,8)}}' | sort > eventos.txt
 
-#awk '{print $1}' eventos_temp.txt > eventos.txt
-while read evento; do echo -n "+ $evento "; grep -c $evento calendario.txt; done < eventos.txt | grep " 0" | awk '{print $1 " " $2}'
-while read evento; do echo -n "- $evento "; grep -c $evento eventos.txt; done < calendario.txt | grep " 0" | awk '{print $1 " " $2}'
+comm -1 -3 calendario.txt eventos.txt
 
-#rm eventos_temp.txt
-rm eventos.txt
 rm calendario.txt
+rm eventos.txt
